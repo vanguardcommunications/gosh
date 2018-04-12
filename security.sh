bad38494c0f884267d0a52fcb39fb8c5407ef1e9
Jonathan Stanley <jonathan@bristleconeweb.com>
2018-04-12 00:25:54 +0000

    notice of lock message

--- security.sh
+++ security.sh
@@ -2,6 +2,7 @@ echo "Administrator password required to enable FileVault"
 sudo fdesetup enable
 echo "Administrator password required to set lost and found notice"
 sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "If found, please contact Vanguard Communications: 303-382-2999, newrequest@vanguardcommunications.net"
+echo "Your Login Window Text can be edited in System Preferences > Security & Privacy > General > Set Lock Message."
 
 # Log installation events for 10 years
 sudo perl -p -i -e 's/format=bsd/format=bsd mode=0640 rotate=utc compress file_max=5M ttl=3650/g' "/etc/asl/com.apple.install"

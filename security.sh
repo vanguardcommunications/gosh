6135397e2800de9b1ca57d3b7e713af3b430cc77
Jonathan Stanley <jonathan@bristleconeweb.com>
2018-04-12 00:15:03 +0000

    security.sh created

---
+++ security.sh
@@ -0,0 +1,13 @@
+echo "Administrator password required to enable FileVault"
+sudo fdesetup enable
+echo "Administrator password required to set lost and found notice"
+sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "If found, please contact Vanguard Communications: 303-382-2999, newrequest@vanguardcommunications.net"
+
+# Log installation events for 10 years
+sudo perl -p -i -e 's/format=bsd/format=bsd mode=0640 rotate=utc compress file_max=5M ttl=3650/g' "/etc/asl/com.apple.install"
+
+# Disable the crash reporter
+defaults write com.apple.CrashReporter DialogType -string "none"
+
+# Check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1

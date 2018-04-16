7c3150ab375156b93b20c9a96c430ea33f8fbe2e
Jonathan Stanley <jonathan@bristleconeweb.com>
2018-04-16 20:57:07 +0000

    fix error; adding quotes per https://stackoverflow.com/questions/45257532/passing-parentheses-as-script-argument-to-macos-defaults-command#45258122

--- lostfound.sh
+++ lostfound.sh
@@ -1,4 +1,4 @@
 # CFR §164.310(c) & §164.310(d)(2)(iii)
 LFMESSAGE="If found, please contact Vanguard Communications: 303-382-2999 or newrequest@vanguardcommunications.net"
-sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText $LFMESSAGE
+sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "$LFMESSAGE"
 echo "Your Login Window Text can be edited in System Preferences > Security & Privacy > General > Set Lock Message."

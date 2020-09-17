2de78a59ace31917fec6b35c6f4286a0fcf4d3b3
Jonathan Stanley <jonathanstanley@users.noreply.github.com>
2020-09-17 17:40:03 -0400

    Update security.sh

--- security.sh
+++ security.sh
@@ -1,3 +1,3 @@
-sh <(curl -s https://bitbucket.org/vcg/gosh/raw/master/lostfound.sh)
-sh <(curl -s https://bitbucket.org/vcg/gosh/raw/master/id.sh)
+sh <(curl -s https://raw.githubusercontent.com/vanguardcommunications/gosh/master/lostfound.sh)
+sh <(curl -s https://raw.githubusercontent.com/vanguardcommunications/gosh/master/id.sh)
 sh <(curl -s https://raw.githubusercontent.com/hipaacert/macos-hipaa/master/comply.sh)
